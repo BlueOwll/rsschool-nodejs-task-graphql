0156b0dec976b7c8aad1a0ba7780d4dc11e96bec
BlueOwll <blueowl.n@gmail.com>
2023-01-29 21:26:42 +0300

    feat: implement  Get users, profiles, posts, memberTypes

---
+++ src/routes/graphql/ggl-types.ts
@@ -0,0 +1,111 @@
+import { GraphQLObjectType, GraphQLNonNull, GraphQLString, GraphQLList, GraphQLInt } from "graphql";
+
+// export type UserEntity = {
+//   id: string;
+//   firstName: string;
+//   lastName: string;
+//   email: string;
+//   subscribedToUserIds: string[];
+// };
+export const user = new GraphQLObjectType({
+  name: 'user',
+  fields: () => ({
+    id: {
+      type: new GraphQLNonNull(GraphQLString),
+    },
+    firstName: {
+      type: GraphQLString,
+    },
+    lastName: {
+      type: GraphQLString,
+    },
+    email: {
+      type: GraphQLString,
+    },
+    subscribedToUserIds: {
+      type: new GraphQLList(GraphQLString),
+    },
+  }),
+});
+// export type ProfileEntity = {
+//   id: string;
+//   avatar: string;
+//   sex: string;
+//   birthday: number;
+//   country: string;
+//   street: string;
+//   city: string;
+//   memberTypeId: string;
+//   userId: string;
+// };
+export const profile = new GraphQLObjectType({
+  name: 'profile',
+  fields: () => ({
+    id: {
+      type: new GraphQLNonNull(GraphQLString),
+    },
+    avatar: {
+      type: GraphQLString,
+    },
+    sex: {
+      type: GraphQLString,
+    },
+    birthday: {
+      type: GraphQLInt,
+    },
+    country: {
+      type: GraphQLString,
+    },
+    city: {
+      type: GraphQLString,
+    },
+    memberTypeId: {
+      type: GraphQLString,
+    },
+    userId: {
+      type: new GraphQLNonNull(GraphQLString),
+    },
+   }),
+});
+// export type PostEntity = {
+//   id: string;
+//   title: string;
+//   content: string;
+//   userId: string;
+// };
+export const post = new GraphQLObjectType({
+  name: 'post',
+  fields: () => ({
+    id: {
+      type: new GraphQLNonNull(GraphQLString),
+    },
+    title: {
+      type: GraphQLString,
+    },
+    content: {
+      type: GraphQLString,
+    },
+    userId: {
+      type: new GraphQLNonNull(GraphQLString),
+    },
+  })
+});
+// export type MemberTypeEntity = {
+//   id: string;
+//   discount: number;
+//   monthPostsLimit: number;
+// };
+export const memberType = new GraphQLObjectType({
+  name: 'memberType',
+  fields: () => ({
+    id: {
+      type: new GraphQLNonNull(GraphQLString),
+    },
+    discount: {
+      type: GraphQLInt,
+    },
+    monthPostLimit: {
+      type: GraphQLInt,
+    },
+  })
+});
--- src/routes/graphql/index.ts
+++ src/routes/graphql/index.ts
@@ -1,4 +1,6 @@
 import { FastifyPluginAsyncJsonSchemaToTs } from '@fastify/type-provider-json-schema-to-ts';
+import { graphql, GraphQLList, GraphQLObjectType, GraphQLSchema } from 'graphql';
+import { user, profile, post, memberType } from './ggl-types';
 import { graphqlBodySchema } from './schema';
 
 const plugin: FastifyPluginAsyncJsonSchemaToTs = async (
@@ -11,8 +13,44 @@ const plugin: FastifyPluginAsyncJsonSchemaToTs = async (
         body: graphqlBodySchema,
       },
     },
-    async function (request, reply) {}
+    async function (request, reply) {
+      //--
+      const RootQuery = new GraphQLObjectType({
+        name: 'query',
+        fields: {
+          users: {
+            type: new GraphQLList(user),
+            resolve () {
+              return fastify.db.users.findMany();
+            },
+          },
+          profiles: {
+            type: new GraphQLList(profile),
+            resolve() {
+              return fastify.db.profiles.findMany();
+            },
+          },
+          posts: {
+            type: new GraphQLList(post),
+            resolve() {
+              return fastify.db.posts.findMany();
+            },
+          },
+          memberTypes: {
+            type: new GraphQLList(memberType),
+            resolve() {
+              return fastify.db.memberTypes.findMany();
+            },
+          },
+        }}
+      ); 
+      const schema = new GraphQLSchema({ query: RootQuery })
+      console.log("schema");
+      return await graphql({ schema, source: request.body.query! });
+    }
   );
-};
+      //--
+    }
 
+// GraphQLFieldResolver<TSource, { [argName: string]: any }, TContext> 
 export default plugin;
